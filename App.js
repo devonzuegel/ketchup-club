5dd2f357781e8706713d892d5894dfbd0fbf574b
Devon Zuegel <devonzuegel@gmail.com>
2023-09-05 14:25:57 -0400

    added map; builds on ios but does not show up

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import {StyleSheet, Text, View} from 'react-native'
+
+// TODO: only call this when  ios/android; move to different file
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'
 
 export default function App() {
+  let location = {
+    latitude: 23.259933,
+    longitude: 77.412613,
+    latitudeDelta: 0.009,
+    longitudeDelta: 0.009,
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>---------</Text>
+      <Text>MapView:</Text>
+
+      <Text>{JSON.stringify(StyleSheet.absoluteFill, null, 2)}</Text>
+
+      <MapView
+        styles={styles.map}
+        provider={PROVIDER_GOOGLE}
+        mapType="hybrid"
+        region={location}>
+        <Marker coordinate={{latitude: 23.25, longitude: 77.41}} />
+      </MapView>
+
+      {/* <StatusBar style="auto" /> */}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +40,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    border: '1px solid red',
+    height: 400,
+    width: 400,
+  },
+})
