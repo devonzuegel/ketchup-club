3358d12329c942272d832bee55d953f6c6648fcf
Devon Zuegel <devonzuegel@gmail.com>
2023-11-10 21:23:25 -0500

    add navigation

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import React, {useState} from 'react'
 import {useFonts} from 'expo-font'
 import {LoginScreen, AuthTokenContext, logout} from './Login'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
 // import Map from './Map'
 
 const containerPadding = 16
@@ -15,6 +18,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     padding: containerPadding,
     paddingTop: 48,
+    borderColor: '#222',
+    borderStyle: 'dotted',
+    borderWidth: 2,
   },
 })
 
@@ -32,27 +38,67 @@ const styles = StyleSheet.create({
 //   )
 // }
 
-const Settings = () => {
-  const {authToken, setAuthToken} = React.useContext(AuthTokenContext)
-  // const [authToken, setAuthToken] = useState(null)
-  const [fontsLoaded] = useFonts(fonts)
-  if (!fontsLoaded) return null
+// const SettingsScreen = () => {
+//   const {authToken, setAuthToken} = React.useContext(AuthTokenContext)
+//   // const [authToken, setAuthToken] = useState(null)
+//   const [fontsLoaded] = useFonts(fonts)
+//   if (!fontsLoaded) return null
 
-  const logout = () => {
-    console.log('logout')
-    setAuthToken(null)
-    AsyncStorage.removeItem('authToken')
-  }
+//   const logout = () => {
+//     console.log('logout')
+//     setAuthToken(null)
+//     AsyncStorage.removeItem('authToken')
+//   }
 
+//   return (
+//     <AuthTokenContext.Provider value={{authToken, setAuthToken}}>
+//       <View>
+//         <Button title="Logout" onPress={logout} />
+//         <Text>Hello, world!</Text>
+//       </View>
+//     </AuthTokenContext.Provider>
+//   )
+// }
+
+function SettingsScreen({navigation}) {
   return (
-    <AuthTokenContext.Provider value={{authToken, setAuthToken}}>
-      <View>
-        <Button title="Logout" onPress={logout} />
-        <Text>Hello, world!</Text>
-      </View>
-    </AuthTokenContext.Provider>
+    <View style={styles.container}>
+      <Text>Settings</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => {
+          /* 1. Navigate to the Home route with params */
+          navigation.navigate('Home', {itemId: 86, otherParam: 'foo'})
+        }}
+      />
+    </View>
   )
 }
+
+function HomeScreen({route, navigation}) {
+  /* 2. Get the param */
+  const {itemId, otherParam} = route.params
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Pre data={route.params} />
+      <Button title="Go to Home... again" onPress={() => navigation.push('Home', {itemId: Math.floor(Math.random() * 100)})} />
+      <Button title="Go to Settings" onPress={() => navigation.navigate('Settings')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator()
+
+const LoggedInNavigator = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} initialParams={{itemId: 10}} />
+      <Stack.Screen name="Settings" component={SettingsScreen} options={{headerShown: false}} />
+    </Stack.Navigator>
+  </NavigationContainer>
+)
 
 export default function App() {
   const [authToken, setAuthToken] = useState(null)
@@ -64,7 +110,8 @@ export default function App() {
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
           <Pre data={{authToken}} />
-          {authToken ? <Settings /> : <LoginScreen />}
+          <LoggedInNavigator />
+          {/* {authToken ? <LoggedInNavigator /> : <LoginScreen />} */}
           {/* <Map /> */}
         </View>
       </TouchableWithoutFeedback>
