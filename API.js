e895d6262db082fdcbc977663ba240505ab55b32
Devon Zuegel <devonzuegel@gmail.com>
2023-11-28 23:25:54 -0500

    use PRODUCTION_URL in prod

--- API.js
+++ API.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: 'https://13fc-132-147-43-111.ngrok-free.app/api/v2',
-})
+const DEVELOPMENT_URL = 'https://13fc-132-147-43-111.ngrok-free.app/api/v2'
+const PRODUCTION_URL = 'https://smallworld.kiwi/api/v2'
+const baseURL = __DEV__ ? DEVELOPMENT_URL : PRODUCTION_URL
+
+console.log('__DEV__:', __DEV__)
+console.log('baseURL:', baseURL)
+
+const api = axios.create({baseURL})
 
 export default api
