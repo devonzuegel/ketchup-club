9d45980986d423999edd117cfbe4173f77adab8e
Devon Zuegel <devonzuegel@gmail.com>
2024-02-13 21:03:02 -0500

    update ngrok url

--- API.js
+++ API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const DEVELOPMENT_URL = 'http://localhost:3001/ketchup/api/v2';
-const DEVELOPMENT_URL = 'https://9bf1-132-147-43-111.ngrok-free.app/ketchup/api/v2'
+const DEVELOPMENT_URL = 'https://4545-132-147-43-111.ngrok-free.app/ketchup/api/v2'
 const PRODUCTION_URL = 'https://smallworld.kiwi/ketchup/api/v2'
 const baseURL = __DEV__ ? DEVELOPMENT_URL : PRODUCTION_URL
 
